53eb220d5c6547cde28491b0ab504644f3dbbbb5
dylanbmk <dylancho0210@gmail.com>
2025-08-08 16:11:04 -0500

    variables

--- src/main/java/co/edu/unbosque/beans/DispositivosElectronico.java
+++ src/main/java/co/edu/unbosque/beans/DispositivosElectronico.java
@@ -1,5 +1,84 @@
 package co.edu.unbosque.beans;
 
-public class DispositivosElectronico {
+public class DispositivosElectronico  extends Articulo{
 
+	private String marca;
+	private int ram;
+	private int almacenamiento;
+	
+	
+	public DispositivosElectronico() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public DispositivosElectronico(String marca, int ram, int almacenamiento) {
+		super();
+		this.marca = marca;
+		this.ram = ram;
+		this.almacenamiento = almacenamiento;
+	}
+
+
+	public DispositivosElectronico(String nombre, int id, String descripcion, float precio, String marca, int ram,
+			int almacenamiento) {
+		super(nombre, id, descripcion, precio);
+		this.marca = marca;
+		this.ram = ram;
+		this.almacenamiento = almacenamiento;
+	}
+
+
+	public DispositivosElectronico(String nombre, int id, String descripcion, float precio) {
+		super(nombre, id, descripcion, precio);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public int getRam() {
+		return ram;
+	}
+
+
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+
+
+	public int getAlmacenamiento() {
+		return almacenamiento;
+	}
+
+
+	public void setAlmacenamiento(int almacenamiento) {
+		this.almacenamiento = almacenamiento;
+	}
+
+
+	@Override
+	public String toString() {
+		return "DispositivosElectronico [marca=" + marca + ", ram=" + ram + ", almacenamiento=" + almacenamiento + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/co/edu/unbosque/beans/JuegoDeMesa.java
+++ src/main/java/co/edu/unbosque/beans/JuegoDeMesa.java
@@ -1,5 +1,88 @@
 package co.edu.unbosque.beans;
 
-public class JuegoDeMesa {
+public class JuegoDeMesa extends Juguete{
+	
+	private int numeroJugadore;
+	private String tipoDeJuego;
+	
+	
+	
+	public JuegoDeMesa() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public JuegoDeMesa(int numeroJugadore, String tipoDeJuego) {
+		super();
+		this.numeroJugadore = numeroJugadore;
+		this.tipoDeJuego = tipoDeJuego;
+	}
+
+
+
+	public JuegoDeMesa(String nombre, int id, String descripcion, float precio, String tamano, String material,
+			int numeroJugadore, String tipoDeJuego) {
+		super(nombre, id, descripcion, precio, tamano, material);
+		this.numeroJugadore = numeroJugadore;
+		this.tipoDeJuego = tipoDeJuego;
+	}
+
+
+
+	public JuegoDeMesa(String nombre, int id, String descripcion, float precio, String tamano, String material) {
+		super(nombre, id, descripcion, precio, tamano, material);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public JuegoDeMesa(String nombre, int id, String descripcion, float precio) {
+		super(nombre, id, descripcion, precio);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public JuegoDeMesa(String tamano, String material) {
+		super(tamano, material);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getNumeroJugadore() {
+		return numeroJugadore;
+	}
+
+
+
+	public void setNumeroJugadore(int numeroJugadore) {
+		this.numeroJugadore = numeroJugadore;
+	}
+
+
+
+	public String getTipoDeJuego() {
+		return tipoDeJuego;
+	}
+
+
+
+	public void setTipoDeJuego(String tipoDeJuego) {
+		this.tipoDeJuego = tipoDeJuego;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "JuegoDeMesa [numeroJugadore=" + numeroJugadore + ", tipoDeJuego=" + tipoDeJuego + "]";
+	}
+	
+	
+	
+	
+	
 
 }
